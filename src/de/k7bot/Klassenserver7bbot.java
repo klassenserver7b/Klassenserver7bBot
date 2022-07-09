cf10835a12361e01ae77f2f6d35e281f9efff31c
klassenserver7b <klassenserver7bwin10@gmail.com>
2022-07-09 23:18:34 +0200

    Added NEWHelpCommand
    Feature Changes
    - Added NEWHelpCommand, which is only usable in Canary Mode, to test the new Help System
    - Added new Help System - now `-help` requires a category to show commands
    
    Bug Fixes
    ...
    
    Maven Changes
    ...

--- src/de/k7bot/Klassenserver7bbot.java
+++ src/de/k7bot/Klassenserver7bbot.java
@@ -17,7 +17,7 @@ import com.sedmelluq.discord.lavaplayer.source.vimeo.VimeoAudioSourceManager;
 import com.sedmelluq.discord.lavaplayer.source.youtube.YoutubeAudioSourceManager;
 
 import core.GLA;
-
+import de.k7bot.commands.NEWHelpCommand;
 import de.k7bot.hypixel.HypixelCommandManager;
 import de.k7bot.listener.*;
 import de.k7bot.manage.*;
@@ -253,6 +253,7 @@ public class Klassenserver7bbot {
 
 		this.shardMan = builder.build();
 
+		NEWHelpCommand.updateCategoryList();
 		InitializeMusic(this.audioPlayerManager);
 		AudioSourceManagers.registerRemoteSources(this.audioPlayerManager);
 		AudioSourceManagers.registerLocalSource(this.audioPlayerManager);
