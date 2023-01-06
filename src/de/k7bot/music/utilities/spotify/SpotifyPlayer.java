867608c32e587d0c7614c8f9764725da900a4aa2
klassenserver <klassenserver7bwin10@gmail.com>
2023-01-06 22:52:55 +0100

    Improved Codefactor v2
    Feature Changes
    ...
    
    Bug Fixes
    ...
    
    Maven Changes
    ...
    
    Known Issues
    ...

--- src/de/k7bot/music/utilities/spotify/SpotifyPlayer.java
+++ src/de/k7bot/music/utilities/spotify/SpotifyPlayer.java
@@ -3,7 +3,6 @@ package de.k7bot.music.utilities.spotify;
 public class SpotifyPlayer {
 
 	public SpotifyPlayer() {
-		// TODO Auto-generated constructor stub
 	}
 	
 	public native void decryptDRM();
