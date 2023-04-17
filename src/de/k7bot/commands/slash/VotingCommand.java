8c600b8623299538aa8f98e3155653b0e4221096
klassenserver7b <79657220+klassenserver7b@users.noreply.github.com>
2023-04-17 22:17:27 +0200

    TITLE
    Feature Changes
    - made VoteReactionListener.java an extra file
    
    - bumped project version to 1.17.0-RC.2
    
    Bug Fixes
    - some readability and l to L changes
    
    Maven Changes
    ...
    
    Known Issues
    ...

--- src/de/k7bot/commands/slash/VotingCommand.java
+++ src/de/k7bot/commands/slash/VotingCommand.java
@@ -6,18 +6,16 @@ package de.k7bot.commands.slash;
 import java.awt.Color;
 import java.time.OffsetDateTime;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 
 import de.k7bot.Klassenserver7bbot;
 import de.k7bot.commands.types.TopLevelSlashCommand;
+import de.k7bot.listener.VoteReactionListener;
 import de.k7bot.util.GenericMessageSendHandler;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.entities.emoji.Emoji;
-import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
-import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.InteractionHook;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
@@ -44,7 +42,7 @@ public class VotingCommand implements TopLevelSlashCommand {
 	@Override
 	public void performSlashCommand(SlashCommandInteraction event) {
 
-		if (!(event.getUser().getIdLong() == 675828196389683223l || event.getUser().getIdLong() == 672514862101954570l
+		if (!(event.getUser().getIdLong() == 675828196389683223L || event.getUser().getIdLong() == 672514862101954570L
 				|| event.getUser().getIdLong() == Klassenserver7bbot.getInstance().getOwnerId())) {
 			event.replyEmbeds(
 					new EmbedBuilder().setColor(Color.red).setDescription("You are not allowed to do this").build())
@@ -87,9 +85,8 @@ public class VotingCommand implements TopLevelSlashCommand {
 
 		event.getUser().openPrivateChannel().complete().sendMessageEmbeds(builder.build()).queue();
 
-		Klassenserver7bbot.getInstance().getShardManager()
-				.addEventListener(new VoteReactionListener(mess.getIdLong(), event.getUser(), names,
-						new GenericMessageSendHandler(hook)));
+		Klassenserver7bbot.getInstance().getShardManager().addEventListener(new VoteReactionListener(mess.getIdLong(),
+				event.getUser(), names, new GenericMessageSendHandler(hook)));
 
 	}
 
@@ -103,67 +100,6 @@ public class VotingCommand implements TopLevelSlashCommand {
 		}
 
 		return Commands.slash("voting", "starts a voting").addOptions(options);
-	}
-
-}
-
-class VoteReactionListener extends ListenerAdapter {
-
-	private Long messageId;
-	private User gameMaster;
-	private LinkedHashMap<String, User> users;
-	private HashMap<User, String> votings;
-	private GenericMessageSendHandler channel;
-
-	public VoteReactionListener(Long messageId, User gameMaster, LinkedHashMap<String, User> users,
-			GenericMessageSendHandler channel) {
-		this.messageId = messageId;
-		this.users = users;
-		this.gameMaster = gameMaster;
-		this.channel = channel;
-		votings = new HashMap<>();
-	}
-
-	@Override
-	public void onMessageReactionAdd(MessageReactionAddEvent event) {
-
-		if (event.getMessageIdLong() != messageId) {
-			return;
-		}
-
-		User u = event.getUser();
-
-		if (event.getGuild().getSelfMember().getUser().getIdLong() == u.getIdLong()) {
-			return;
-		}
-
-		if (!votings.containsKey(u) && users.values().contains(u)) {
-			votings.put(u, event.getEmoji().getName());
-		}
-
-		event.getReaction().removeReaction(u).queue();
-
-		if (votings.size() >= users.size()) {
-
-			StringBuilder build = new StringBuilder();
-
-			votings.forEach((user, emojiname) -> {
-
-				build.append(user.getName());
-				build.append(" - ");
-				build.append(emojiname);
-				build.append("\n");
-
-			});
-
-			channel.sendMessage("Voting beendet!");
-			gameMaster.openPrivateChannel().complete()
-					.sendMessageEmbeds(new EmbedBuilder().setDescription(build.toString()).build()).queue();
-
-			Klassenserver7bbot.getInstance().getShardManager().removeEventListener(this);
-
-		}
-
 	}
 
 }
