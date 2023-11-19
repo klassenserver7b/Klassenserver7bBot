6027325f5ec7e5375a6207c60b7c984c96742b5a
K7 <klassenserver7bwin10@mail.com>
2023-11-19 01:45:47 +0100

    k7todo

--- src/main/java/de/klassenserver7b/k7bot/listener/MessageListener.java
+++ src/main/java/de/klassenserver7b/k7bot/listener/MessageListener.java
@@ -3,9 +3,13 @@
  */
 package de.klassenserver7b.k7bot.listener;
 
+import java.util.Date;
+
 import javax.annotation.Nonnull;
 
+import de.klassenserver7b.k7bot.sql.LiteSQL;
 import net.dv8tion.jda.api.events.message.MessageDeleteEvent;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 /**
@@ -15,7 +19,25 @@ public class MessageListener extends ListenerAdapter {
 
 	@Override
 	public void onMessageDelete(@Nonnull MessageDeleteEvent event) {
-		
+
+	}
+
+	@Override
+	public void onMessageReceived(@Nonnull MessageReceivedEvent event) {
+
+		// FIXME enable logging
+		boolean loggingenabled = false;
+
+		if (loggingenabled) {
+
+			LiteSQL.onUpdate("INSERT INTO messagelogs(messageId, guildId, date, messageText) VALUES(?,?,?)",
+					event.getMessageIdLong(),
+					(event.getGuild() == null ? 0 : event.getGuild().getIdLong()),
+					new Date().getTime(),
+					event.getMessage().getContentRaw());
+
+		}
+
 	}
 
 }
